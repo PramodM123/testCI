c5de148568989f399bdf1d3e5afdd5479f788c3c
PramodM123 <m.pramodreddy123@gmail.com>
2017-06-04 18:04:02 -0700

    New code with JUnits

---
+++ src/main/java/com/pramod/PramodMulTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.pramod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Pramod Mekapothula
+ *
+ */
+public class PramodMulTest {
+ 
+	  @Test(expected = IllegalArgumentException.class)
+	  public void testExceptionIsThrown() {
+	    PramodMul tester = new PramodMul();
+	    tester.multiply(1000, 5);
+	  }
+
+	  @Test
+	  public void testMultiply() {
+		  PramodMul tester = new PramodMul();
+	    assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+	  }
+
+	  @Test(expected = IllegalArgumentException.class)
+	  public void testExceptionIsThrowned()
+	  {
+		  PramodMul pram = new PramodMul();
+		  pram.division(1000, 0);
+	  }
+	  
+	  @Test
+	  public void testDiv() {
+		  PramodMul pram = new PramodMul();
+		assertEquals("10 / 5 must be 2", 2 , pram.division(10, 5)); 
+		}
+}
